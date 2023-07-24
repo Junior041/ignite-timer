36a85ff48e6567c90028178c7fa2dc7b3409ff57
Junior <ismaeljunior1508@gmail.com>
2023-07-24 17:51:14 -0300

    localStorage

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -1,4 +1,10 @@
-import { createContext, ReactNode, useReducer, useState } from 'react';
+import {
+  createContext,
+  ReactNode,
+  useEffect,
+  useReducer,
+  useState,
+} from 'react';
 import { Cycle, cyclesReducer } from '../reducers/cycles/reducer';
 import {
   ActionTypes,
@@ -6,6 +12,7 @@ import {
   interruptCurrentCycleAction,
   markCurrentCycleAsFinishedAction,
 } from '../reducers/cycles/actions';
+import { differenceInSeconds } from 'date-fns';
 
 interface CreateCycleData {
   task: string;
@@ -32,20 +39,37 @@ interface CyclesContextProviderProps {
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
-  const [cyclesState, dispatch] = useReducer(cyclesReducer, {
-    cycles: [],
-    activeCycleId: null,
-  });
+  const [cyclesState, dispatch] = useReducer(
+    cyclesReducer,
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+    (initialState) => {
+      const storedStateAsJson = localStorage.getItem(
+        '@ignite-timer:cycles-state-1.0.0',
+      );
+      if (storedStateAsJson) {
+        return JSON.parse(storedStateAsJson);
+      }
 
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+      return initialState;
+    },
+  );
+  const { cycles, activeCycleId } = cyclesState;
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(() => {
+    if (activeCycle) {
+      return differenceInSeconds(new Date(), new Date(activeCycle.startDate));
+    }
+    return 0;
+  });
 
   useEffect(() => {
     const stateJSON = JSON.stringify(cyclesState);
     localStorage.setItem('@ignite-timer:cycles-state-1.0.0', stateJSON);
   }, [cyclesState]);
-
-  const { cycles, activeCycleId } = cyclesState;
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
   function setSecondsPassed(seconds: number) {
     setAmountSecondsPassed(seconds);
